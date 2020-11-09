0af2fd96af0962d0680ca93bce397153507ffe0a
Muhammad Yusuf <73867726+yusufthx@users.noreply.github.com>
2020-11-09 16:24:39 +0800

    Code for example 7 (Week 4)
    There are two LEDs whose anodes are connected to PB0 and PB1 respectively. The LED1 needs to be flashing with ON time 750ms and OFF time 350ms.
    The LED2 needs to be flashing with ON time 400 and OFF time 600ms.

---
+++ example_8_w4.ino
@@ -0,0 +1,27 @@
+ //this code to blink two led in two differernt rate
+ //There are two LEDs whose anodes are connected to PB0 and PB1 respectively. 
+ //The LED0 needs to be flashing with ON time 750ms and OFF time 350ms.
+//The LED1 needs to be flashing with ON time 400 and OFF time 600ms.
+  
+  int led0= 0;
+  int led1= 1;
+
+void setup() {
+  pinMode(led0, OUTPUT);
+  pinMode(led1, OUTPUT);
+  
+}
+
+void loop() {
+  //led0
+  digitalWrite(led0, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(750);                       // wait for 750ms 
+  digitalWrite(led0, LOW);    // turn the LED off by making the voltage LOW
+  delay(350);                       // wait for 350ms
+
+  //led1
+  digitalWrite(led0, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(400);                       // wait for 400ms 
+  digitalWrite(led0, LOW);    // turn the LED off by making the voltage LOW
+  delay(600);                       // wait for 600ms
+}
